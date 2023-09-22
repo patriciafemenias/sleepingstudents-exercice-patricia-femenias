9e8c0d0ccacc59cbab93dba4833f651200ee86fd
Patricia <patricia.femenias@gmail.com>
2023-09-22 13:48:13 +0200

    Add introduceYourself function

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const studentA = {
   surname: "Elisei",
   lastNightSleepingHours: 7,
   grade: 9,
+  introduceYourself() {
+    return `Me llamo ${this.name} ${this.surname} y esta noche he dormido ${this.lastNightSleepingHours} horas`;
+  },
 };
+
+console.log(studentA.introduceYourself());
